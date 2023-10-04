7cb990245fb1cde43645ad64f10e276897f6fa2a
JanB-n <-v>
2023-10-04 04:25:36 +0200

    new changes

---
+++ relacsMongo/helpers.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd 
+import json
+import math
+from io import StringIO 
+
+#COLUMNS = ['Temperature (K)', 'Magnetic Field (Oe)', "AC X' (emu/Oe)", "AC X'' (emu/Oe)", "AC Frequency (Hz)"]
+COLUMNS = ['Temperature', 'MagneticField', 'ChiPrime', 'ChiBis', 'Frequency']
+NEW_COLUMNS = {'Temperature': 'Temperature (K)', 'MagneticField': 'Magnetic Field (Oe)', 'ChiPrime': "AC X' (emu/Oe)", 'ChiBis': "AC X'' (emu/Oe)", 'Frequency': "AC Frequency (Hz)"}
+
+def mergeLists(list1, list2):
+    return list(np.unique(list1, list2))
+
+def get_data_from_csv(measurements_csv):
+    
+    data_file = StringIO(measurements_csv)
+    data = pd.read_csv(data_file)
+    data = data.sort_values(NEW_COLUMNS['Temperature'])
+    columns_swapped = {value:key for key, value in NEW_COLUMNS.items()}
+    data = data.rename(columns=columns_swapped)
+    data = data[COLUMNS]
+    return data
+   
+def calculate_additional_measurements(data, molar_mass, probe_mass):
+    
+    data["ChiPrimeMol"] = data["ChiPrime"] * molar_mass/probe_mass
+    data["ChiBisMol"] = data["ChiBis"] * molar_mass/probe_mass
+    data["Omega"] = 2 * data["Frequency"] * np.pi
+    data["OmegaLog"] = np.log10(data["Omega"])
+    data["FrequencyLog"] = np.log10(data["Frequency"])
+
+    data = data.sort_values('Temperature')
+
+    return data
+
+def clusterize(data, field_epsilon, tmp_epsilon):
+    fields = cluster(data, "MagneticField", field_epsilon)
+    for i in list(range(0, len(fields))):
+            fields[i] = cluster(fields[i], "Temperature", tmp_epsilon)
+    return fields
+
+def cluster(data, by, epsilon, reindex=False):
+    sorted_data = data.sort_values(by=by).copy()
+    min_value = sorted_data[by].min()
+    results = []
+    df = pd.DataFrame(columns=data.columns)
+
+    for _, row in sorted_data.iterrows():
+            if row[by] <= min_value + epsilon:
+                df.loc[-1] = row
+                df.index = df.index + 1
+            else:
+                results.append(df)
+                df = pd.DataFrame(columns=data.columns)
+                min_value = row[by]
+                df.loc[-1] = row
+                df.index = df.index +1
+        
+    results.append(df)
+
+    if reindex:
+        i:int
+        for i in range(0, len(results)):
+            results[i] = results[i].sort_values(by, ascending=False).reset_index(drop=True)
+
+        # i: int
+    for i in range(0, len(results)):
+            results[i] = results[i].sort_values("Omega")
+    
+    return results
+
+def measurize(measurement, file_name, field_epsilon, tmp_epsilon):
+    accuracy_temp: float = math.log10(tmp_epsilon)
+    if(tmp_epsilon >= 1):
+        accuracy_temp = 0
+    else:
+        accuracy_temp = abs(math.floor(accuracy_temp) + 1)
+
+    accuracy_field: float = math.log10(field_epsilon)
+    if(tmp_epsilon >= 1):
+        accuracy_field = 0
+    else:
+        accuracy_field = abs(math.floor(accuracy_field) + 1)
+        
+    temp = round((measurement["Temperature"].max() + measurement["Temperature"].min())/2, accuracy_temp)
+    field = round((measurement["MagneticField"].max() + measurement["MagneticField"].min())/2, accuracy_field)
+
+    if '.' in file_name:
+        file_name = file_name.split('.')[0]
+
+    name = f"T: {temp}K H: {field}Oe {file_name}"
+    length = len(measurement["Frequency"])
+    measurement["Hidden"] = pd.Series(np.zeros(length), index=measurement.index)
+    
+    return {'name': name, 'df': measurement, 'temp': temp, 'field': field}
+    return Measurement(name, df, temp, field, compound, collection)     
--- relacsMongo/views.py
+++ relacsMongo/views.py
@@ -11,6 +11,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.contrib.auth.models import User
 import json
 from bson import json_util
+from bson.objectid import ObjectId
+from . import helpers
 # Create your views here.
 
 connection_string = 'mongodb+srv://9baran:ArcyWrog@cluster0.hmiq5.mongodb.net/?retryWrites=true&w=majority'
@@ -37,7 +39,7 @@ JWT_authenticator = JWTAuthentication()
 #         serializer.save()
 #     return Response(serializer.data)
 
-class CompoundView(APIView): 
+class CompoundsView(APIView): 
     def post(self, request, format='json'):
         response = JWT_authenticator.authenticate(request)
         if response is not None:
@@ -46,6 +48,7 @@ class CompoundView(APIView):
             #request.data['id_user'] = user 
             newdata = request.data
             newdata['id_user'] = user.pk
+            newdata['measurements'] = []
             # if serializer.is_valid(raise_exception = True):
             #     compound = serializer.save()
             #     if compound:
@@ -65,15 +68,167 @@ class CompoundView(APIView):
     def get(self, request):
         userVerified = JWT_authenticator.authenticate(request)
         if userVerified is not None:
-            compound_list = []
-            for compound in compounds.find({"id_user": request.user.id}):
-                compound_list.append(compound)
-            compounds_serialized = json_util.dumps(compound_list)
-            print(compounds_serialized)
-            #compounds_serialized = serializers.serialize('json', compounds)
-            #json.loads(compounds_serialized)
-            #print(json.loads(compounds_serialized)[0])
-            return Response(json.loads(compounds_serialized), content_type="application/json")
+            try:
+                compound_list = []
+                for compound in compounds.find({"id_user": request.user.id}):
+                    compound_list.append(compound)
+                compounds_serialized = json_util.dumps(compound_list)
+                # print(compounds_serialized)
+                #compounds_serialized = serializers.serialize('json', compounds)
+                #json.loads(compounds_serialized)
+                #print(json.loads(compounds_serialized)[0])
+                return Response(json.loads(compounds_serialized), content_type="application/json")
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
             print("User not verified")
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+class CompoundView(APIView):
+    def get(self, request):
+        userVerified = JWT_authenticator.authenticate(request)
+        id = request.GET.get('id')
+        if userVerified is not None:
+            try:
+                document = compounds.find_one({'_id': ObjectId(id)})
+                document_serialized = json_util.dumps(document)
+                # print(document)
+                if document is not None:
+                    return Response(json.loads(document_serialized), content_type="application/json")
+                else:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        else:
+            print("User not verified")
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+    def post(self, request):
+        response = JWT_authenticator.authenticate(request)
+        if response is not None:
+            # print(request.data['measurements'])
+            # with open('df.txt', 'w') as f:
+            #     f.write(json.dumps(request.data['measurements']))
+            # if serializer.is_valid(raise_exception = True):
+            #     compound = serializer.save()
+            #     if compound:
+            #         json = serializer.data
+            #         return Response(json, status=status.HTTP_201_CREATED)
+            # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            if True:
+            # try:
+            
+                full_data = request.data['measurements']
+                probe_mass = float(request.data['probe_mass'])
+                field_epsilon = 1
+                tmp_epsilon = 0.05
+                file_name =  request.data['file_name']
+                document = compounds.find_one({'_id': ObjectId(request.data['comp_id'])})
+                molar_mass = float(document['molar_mass'])
+                data = helpers.get_data_from_csv(full_data)
+                data = helpers.calculate_additional_measurements(data, molar_mass, probe_mass)
+                clusterized_data = helpers.clusterize(data, field_epsilon, tmp_epsilon)
+                measurements = []
+                for cluster in clusterized_data:
+                    for measurement in cluster:
+                        print(measurement)
+                        measurements.append(helpers.measurize(measurement, file_name, field_epsilon, tmp_epsilon))
+                
+                # for measurement in measurements:
+                #     measurement['df'] = measurement['df'].to_json()
+                #     print(measurement['df'])
+                
+                # for one_measurement in full_data:
+                #     one_measurement = {key: one_measurement[key] for key in ['Temperature (K)', 'Magnetic Field (Oe)', "AC X' (emu/Oe)", "AC X'' (emu/Oe)", "AC Frequency (Hz)"]}
+                #     data.setdefault((round(one_measurement['Temperature (K)'], 1), round(one_measurement['Magnetic Field (Oe)'], 1)), []).append(one_measurement)
+
+                # for key, value in data.items():
+                #     measurement_collection = {}
+                #     measurement_collection['temperature'] = key[0]
+                #     measurement_collection['field'] = key[1]
+                #     measurement_collection['name'] = f'T: {key[0]}K H: {key[1]}e'
+                #     measurement_collection['measurements_local'] = value
+                #     measurements.append(measurement_collection)
+
+                document_serialized = json_util.dumps(document)
+                document = json.loads(document_serialized)
+                # if 'measurements' in document:
+                #     #new_measurements = helpers.mergeLists(document['measurements'], measurements)
+                #     #compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": new_measurements}})
+                #     #compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": {}}})
+                #     compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": measurements}})
+                # else:
+                #     compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": measurements}})
+                # document = compounds.find_one({'_id': ObjectId(request.data['comp_id'])})
+                # document_serialized = json_util.dumps(document)
+                # document = json.loads(document_serialized)
+
+                # with open('document.json', 'w') as f:
+                #     f.write(json.dumps(measurements))
+                
+                if 'measurements' in document != [] :
+                    saved_measurements = document['measurements']
+                    for new_measurement in measurements:
+                        theSame = False
+                        for document_measurement in document['measurements']:
+                            if new_measurement['name'] == document_measurement['name']:
+                                theSame = True
+                        if theSame == False:
+                            new_measurement['df'] = new_measurement['df'].to_json()
+                            saved_measurements.append(new_measurement)
+                    compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": saved_measurements}})
+                else:
+                    compounds.update_one({"_id":  ObjectId(request.data['comp_id'])},{"$set":{"measurements": measurements}})
+                print('calculated')
+                document = compounds.find_one({'_id': ObjectId(request.data['comp_id'])})
+                document_serialized = json_util.dumps(document)
+                document = json.loads(document_serialized)
+
+                with open('document.json', 'w') as f:
+                    f.write(json.dumps(document))
+            try:        
+                return Response(status=status.HTTP_201_CREATED)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            print("no token is provided in the header or the header is missing")
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+class MeasurementsView(APIView):
+    def get(self, request):
+        userVerified = JWT_authenticator.authenticate(request)
+        id = request.GET.get('id')
+        if userVerified is not None:
+            try:
+                measurements = list(compounds.find({'_id': ObjectId(id)}, {"measurements": 1, "_id": 0}))
+                #document_serialized = json_util.dumps(document)
+                # print(document)
+                if measurements is not None:
+                    return Response(measurements[0]['measurements'], content_type="application/json")
+                else:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+        else:
+            print("User not verified")
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+    def delete(self, request):
+        userVerified = JWT_authenticator.authenticate(request)
+        id = request.GET.get('id')
+        if userVerified is not None:
+            #try:
+                compounds.update_one({"_id":  ObjectId(id)},{"$set":{"measurements": []}})
+                #document_serialized = json_util.dumps(document)
+                # print(document)
+            
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            #except:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    
+        
