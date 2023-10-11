32a8c04b7174016e989264b45bab66239d59fdff
JanB-n <-v>
2023-10-11 20:16:48 +0200

    it works

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xq6fcme+ii-mnrw$qs)k9zw7dnarx6p7gv2ocqn-8!04$!c-va
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['relacsbackend.azurewebsites.net']
+ALLOWED_HOSTS = ['relacsbackend.azurewebsites.net', 'localhost']
 
 
 # Application definition
@@ -86,11 +86,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'neondb',
+    'USER': 'jasiekwarrior',
+    'PASSWORD': '7MoqN4TBVsLx',
+    'HOST': 'ep-quiet-mouse-01102839.eu-central-1.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+  }
 }
 
 
